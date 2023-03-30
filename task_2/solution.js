974a7b1b09ffe4f59f17c4bdfd6c5abfce9ff87a
Diana Pertsovskaya <diana2812pertsovskaya@yandex.ru>
2023-03-30 19:42:56 +0300

    change_2

--- task_2/solution.js
+++ task_2/solution.js
@@ -5,12 +5,12 @@ function calcShipping(sum, min, shipping) {
 
     // Задание №2.1. Рассчитать доставку
 
-    let = shippingSum; // создайте переменную shippingSum
+    let shippingSum; // создайте переменную shippingSum
 
     if(productsSum === 0) { // если productsSum равно 0,
     console.log(shippingSum = 0);// то shippingSum присвоить значение 0
     }
-    if(productsSum >= freeShippingMinSum) { // если productsSum Больше или равна freeShippingMinSum,
+    if(productsSum > freeShippingMinSum || productsSum == freeShippingMinSum) { // если productsSum Больше или равна freeShippingMinSum,
     console.log(shippingSum = 0);// то shippingSum присвоить значение 0
     }   
     if(productsSum > 0 && productsSum < freeShippingMinSum) { // если productsSum больше 0 и меньше freeShippingMinSum,
